0dc93d40f8c57c2ee96558b2822d19ad74f0cdf2
KonstantinAMP <gijoe403@gmail.com>
2016-04-15 23:41:48 +0400

    add port forwarding fix deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # upgrade aptitude repository
-sudo apt-get upgrade
+sudo apt-get update
 
 # install midnight commander
 sudo apt-get --force-yes -y install mc
@@ -11,18 +11,19 @@ wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-
 
 tar -xf jdk-8u77-linux-x64.tar.gz
 
-# sudo mkdir -p /usr/lib/java/jdk
+sudo mkdir -p /usr/lib/java
 
-sudo mv jdk1.8.0_77 /usr/lib/java/jdk
+sudo mv jdk1.8.0_77 /usr/lib/java/jdk1.8
 
 rm jdk-8u77-linux-x64.tar.gz
 
-echo 'export JAVA_HOME="/usr/lib/java/jdk/jdk1.8.0_77"' >> .bashrc
+echo 'export JAVA_HOME="/usr/lib/java/jdk1.8"' >> .bashrc
 echo 'PATH=:$PATH:$JAVA_HOME/bin' >> .bashrc
 
 # install postgresql
 sudo apt-get --force-yes -y install postgresql
 
+# start postgresql server
 sudo service postgresql start
 
 # insta wildfly
@@ -31,8 +32,6 @@ wget http://download.jboss.org/wildfly/10.0.0.Final/wildfly-10.0.0.Final.tar.gz
 tar -xf wildfly-10.0.0.Final.tar.gz
 
 rm wildfly-10.0.0.Final.tar.gz
-
-# sudo mkdir /server
 
 sudo mv wildfly-10.0.0.Final /server
 
