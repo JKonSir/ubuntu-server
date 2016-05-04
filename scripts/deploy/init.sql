33a2989777808718e6d5e4569c97f96a9edaa6a3
KonstantinAMP <gijoe403@gmail.com>
2016-05-04 12:23:09 +0400

    Update init.sql

--- scripts/deploy/init.sql
+++ scripts/deploy/init.sql
@@ -2,4 +2,4 @@ CREATE DATABASE testdb;
 
 CREATE USER test_user WITH password 'qwerty';
 
-GRANT ALL privileges ON DATABASE test_database TO test_user;
+GRANT ALL privileges ON DATABASE testdb TO test_user;
